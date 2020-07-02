c73a58d38bc93b5ffb79ccbf4f19660ef0c30cc8
kuramoto-k <koutaroukuramoto@gmail.com>
2020-07-02 18:05:59 +0900

    init

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+    <h1 class="text-4xl text-teal-400">
+      tailwind
+    </h1>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+</style>
